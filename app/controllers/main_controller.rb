9e33bf186ff3a05dfee61d71190b969210eb8be3
John Fan <john.fan@friendstock.org>
2011-09-11 13:08:00 -0400

    update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,22 @@
 class MainController < ApplicationController
 
+  require 'rubygems'
+  require 'twilio-ruby'
 
   def index
     
   end
   
   def send_sms
-    
+    @account_sid = 'AC8652de94df71913aa4eeb7e1afd87680'
+    @auth_token = "1eb2f22a32cef348c4fdcd8eb4403165"
+
+    # set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+
+    @to_number = params[:to_number]
+    @body = params[:body]
+    @account = @client.account
+    @message = @account.sms.messages.create({:to => '+16506463891', :from => '+16504927047', :body => @body})  
   end
 end
