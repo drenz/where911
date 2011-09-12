2f90f50330ced9047f243ca37e7f0b98eaf0f9e0
John Fan <john.fan@friendstock.org>
2011-09-12 02:09:20 -0400

    remove auth

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,7 +9,7 @@ class MainController < ApplicationController
   
   def send_sms
     @account_sid = 'AC8652de94df71913aa4eeb7e1afd87680'
-    @auth_token = "1eb2f22a32cef348c4fdcd8eb4403165"
+    @auth_token = ENV["twilio_auth_token"]
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new(@account_sid, @auth_token)
--- app/controllers/scratch_controller.rb
+++ app/controllers/scratch_controller.rb
@@ -5,11 +5,10 @@ class ScratchController < ApplicationController
 
   def sms    
     @account_sid = 'AC8652de94df71913aa4eeb7e1afd87680'
-    @auth_token = "1eb2f22a32cef348c4fdcd8eb4403165"
+    @auth_token = ENV["twilio_auth_token"]
 
     # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
-
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token) 
 
     @account = @client.account
     @message = @account.sms.messages.create({:to => '+16506463891', :from => '+16504927047', :body => "hello"})
